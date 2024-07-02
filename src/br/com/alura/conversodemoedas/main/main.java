6115122c0809277a1bce651a4c681a535669a8e9
Thiago TSL <106386727+Thiago-tsl@users.noreply.github.com>
2024-07-02 15:48:55 -0300

    Update main.java

--- src/br/com/alura/conversodemoedas/main/main.java
+++ src/br/com/alura/conversodemoedas/main/main.java
@@ -15,16 +15,16 @@ public class main {
 
         do {
             System.out.println(menu.getMenu());
-            System.out.println("Escolha a Opção desejada: ");
+            System.out.println("Escolha a sua Opção desejada: ");
             option = reading.nextInt();
             if (option == 0) {
                 break;
             } else if (option >= 1 && option <= 8) {
-                System.out.println("Qual o valor que deseja converter?");
+                System.out.println("Qual é o valor que deseja converter?");
                 valueToConvert = reading.nextDouble();
                 menu.conversionOption(option, valueToConvert);
             } else {
-                System.out.println("Opção invalida\n");
+                System.out.println("essa Opção é invalida\n");
             }
         } while (option != 0);
         menu.showLog();
